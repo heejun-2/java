2f2e3ed8149a68368d6eea16f53e43a96df527bf
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2022-05-01 21:31:56 +0900

    3053 풀이

---
+++ baekjoon/3053/Main_3053.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main_3053 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		double R = sc.nextDouble();
+		
+		System.out.println(R * R * Math.PI);
+		System.out.println(2 * R * R);
+	}
+
+}
