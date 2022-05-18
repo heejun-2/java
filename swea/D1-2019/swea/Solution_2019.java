86fc1ba8d5be7a8af7764b92ea9dd5aba14a7897
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2022-05-18 09:17:54 +0900

    2019 풀이

---
+++ swea/D1-2019/swea/Solution_2019.java
@@ -0,0 +1,19 @@
+package swea;
+
+import java.util.*;
+
+public class Solution_2019 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i = 0; i <= N; i++) {
+			System.out.print((int)Math.pow(2,i)+" ");
+		}
+		
+	}
+
+}
