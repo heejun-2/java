b85a7c40b2daa440e31364b8bffcc6b744a43c6a
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2022-07-11 11:59:19 +0900

    프로그래머스 : 완주하지 못한 선수

---
+++ coding/src/coding/Solution1.java
@@ -0,0 +1,20 @@
+package coding;
+
+import java.util.*;
+
+class Solution1 {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        for(int i = 0; i < completion.length; i++){
+            if(participant[i].equals(completion[i])){
+                continue;
+            }
+            else{
+                return participant[i];
+            }
+        }
+        return participant[participant.length-1];
+    }
+}
