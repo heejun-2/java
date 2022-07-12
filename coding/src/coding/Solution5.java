8327a5e5369f97cca12ba78d5b6260b60e570a18
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2022-07-12 10:03:49 +0900

    프로그래머스 : 평균 구하기

---
+++ coding/src/coding/Solution5.java
@@ -0,0 +1,17 @@
+package coding;
+
+class Solution {
+    public double solution(int[] arr) {
+        
+        double answer = 0;
+        double sum = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        
+        answer = sum / (double) arr.length;
+        
+        return answer;
+    }
+}
