dd1ae914b9cc059cb3f93f3345f39240d45c0580
DESKTOP-OG4PMN3\ghktk <ghktkf012@naver.com>
2022-07-18 10:05:18 +0900

    프로그래머스 : 최소직사각형

---
+++ coding/src/coding/Solution34.java
@@ -0,0 +1,21 @@
+package coding;
+
+class Solution34 {
+    public int solution(int[][] sizes) {
+        int row = 0;
+        int col = 0;
+        
+        for(int i = 0; i < sizes.length; i++){    
+            if(sizes[i][0] > sizes[i][1]){
+                int tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+            row = Math.max(row,sizes[i][0]);
+            col = Math.max(col,sizes[i][1]);
+        }
+        
+        return row * col;
+        
+    }
+}
